d19a306534c724951cb6110e2619c8310f9830df
raJiB <pokhrelrajib016@gmail.com>
2024-04-12 20:07:46 +0545

    set up the apps and index js files

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import cors from "cors"
+import express from "express"
+import cookieParser from "cookie-parser"
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({
+    limit:"16kb"
+})) // to parse the incomming requests with JSON payloads
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+//import the routers
+
+
+export default app;
