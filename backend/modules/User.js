2ad3b2100a5e8abce2f8695636c3dbcd1a72abf4
raJiB <pokhrelrajib016@gmail.com>
2024-04-13 09:33:58 +0545

    added tokens and bcrypt functionalities

---
+++ backend/modules/User.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcryptjs"
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"]
+    },
+    avatar: {
+        type: String,
+        default: ""
+    }
+})
+
+//hash the passwords
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+//check if the password is correct
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema)
