98bb8201749c3b0d544873ea8428e00b4c3c97b8
raJiB <pokhrelrajib016@gmail.com>
2024-04-25 14:34:05 +0545

    added children and outlets to the main page and removed the app.jsx page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from "react-router-dom"
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home, Signup, Login, About, Contact, Projects } from "./pages/index.pages.js";
+import { AuthContextProvider, useAuthContext } from './context/AuthContext.jsx';
+import Footer from './components/footer/footer.jsx'
+import Header from './components/header/Header.jsx'
+
+import './index.css';
+
+function Layout({ children }) {
+  return (
+    <div className='container mx-auto mt-2'>
+      <Header/>
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+function AppRoutes() {
+  const { authUser } = useAuthContext();
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          authUser ? <Home /> : <Navigate to="/login" />
+        }
+      />
+      <Route path="about" element={authUser ? <About /> : <Navigate to="/login" />} />
+      <Route path="contact" element={authUser ? <Contact /> : <Navigate to="/login" />} />
+      <Route path="login" element={authUser ? <Navigate to="/" /> : <Login />} />
+      <Route path="signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
+    </Routes>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </BrowserRouter>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,9 +5,7 @@ import Footer from '../../components/footer/footer'
 function Home() {
   return (
     <div>
-      
-      <Header/>
-      <Footer />
+      <h1>This is the Home page</h1>
     </div>
   )
 }
