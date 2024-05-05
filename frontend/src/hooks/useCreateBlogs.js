8faf47beddcf6fae7e794d427571420465794683
raJiB <pokhrelrajib016@gmail.com>
2024-05-05 18:04:14 +0545

    created hooks and input forms for adding blogs

--- frontend/src/hooks/useCreateBlogs.js
+++ frontend/src/hooks/useCreateBlogs.js
@@ -6,38 +6,38 @@ export const useCreateBlogs = () => {
 
     const [loading, setLoading] = useState(false);
     const { authUser } = useAuthContext()
-    const createBlogs = async ({heading, description, image}) => {
-        const success = handleInputErrors({heading, description, image})
+    const createBlogs = async ({ heading, description, image }) => {
+        const success = handleInputErrors(heading, description, image);
         if (!success) return;
         setLoading(true);
         try {
             const formData = new FormData();
             formData.append("heading", heading);
             formData.append("description", description);
-            formData.append("image",image);
+            formData.append("image", image);
 
-            const res = await fetch("/api/blog/create-blog",{
+            const res = await fetch("/api/blog/create-blog", {
                 method: "POST",
                 body: formData,
             })
 
             const data = await res.json();
-            if(data.error){
+            if (data.error) {
                 throw new Error(data.error)
             }
             console.log(data);
         } catch (error) {
             toast.error(error.message)
-        }finally{
+        } finally {
             setLoading(false)
         }
     };
 
-    return {loading, createBlogs};
+    return { loading, createBlogs };
 }
 
-const handleInputErrors =(heading, description, image)=>{
-    if(!heading || !description || !image){
+const handleInputErrors = (heading, description, image) => {
+    if (!heading || !description || !image) {
         return false;
     }
     return true;
--- frontend/src/pages/insert/Insert.jsx
+++ frontend/src/pages/insert/Insert.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useCreateBlogs } from "../../hooks/useCreateBlogs.js"
 
 function Insert() {
+
+  const initialInputs = {
+    heading: "",
+    description: "",
+    image: null
+  }
+
+  const [inputs, setInputs] = useState(initialInputs);
+  const { loading, createBlogs } = useCreateBlogs();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!inputs.heading || !inputs.description || !inputs.image){
+      alert ("Please fill in all the fields");
+      return;
+    }
+    await createBlogs(inputs);// run hook
+    setInputs(initialInputs); //reset
+    alert("Blog created successfully")
+  }
+
+  //separate function to handle the file upload
+  const handleFileChange = async (e) => {
+    setInputs({ ...inputs, image: e.target.files[0] });
+  }
+
+
   return (
-    <div>Insert</div>
+    <div className='flex flex-col w-[50vw] mx-auto p-4 gap-5'>
+      <h1 className='text-white text-4xl my-3 font-bold'>Create your new Blog!!</h1>
+      <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+        <label className="form-control w-full max-w-md">
+          <div className="label">
+            <span className="label-text text-[1.3vw]">Heading </span>
+          </div>
+          <input type="text" placeholder="Type here" className="input input-bordered input-md w-full max-w-md h-10" value={inputs.heading} onChange={(e) => { setInputs({ ...inputs, heading: e.target.value }) }} />
+        </label>
+        <label className="form-control w-full max-w-md">
+          <div className="label">
+            <span className="label-text text-[1.3vw]">Description</span>
+          </div>
+          <textarea className="textarea textarea-bordered input-md h-24" placeholder="Description" value={inputs.description} onChange={(e) => setInputs({ ...inputs, description: e.target.value })}></textarea>
+        </label>
+        <input type="file" className="file-input file-input-bordered w-full max-w-xs" onChange={handleFileChange} />
+
+        <div>
+        <button className="btn max-w-sm text-white text-xl font-thin bg-blue-800 " disabled={loading}>
+              {loading ? <span className='loading loading-spinner'></span> : "Post"}
+          </button>
+        </div>
+
+      </form>
+    </div>
   )
 }
 
