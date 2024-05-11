1991acec3166a6822a502d7d4d3264243ce7643d
raJiB <pokhrelrajib016@gmail.com>
2024-05-11 09:36:56 +0545

    added hooks for getBlogs

---
+++ frontend/src/hooks/useGetBlogs.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { toast } from 'react-hot-toast'
+import { useAuthContext } from "../context/AuthContext";
+import useBlogs from "../zustand/useBlogs.js";
+
+
+const useGetBlogs = () => {
+    const { authUser } = useAuthContext();
+    const [loading, setLoading] = useState(false);
+    const { blogs, setBlogs } = useBlogs();
+
+    const getBlogs = async () => {
+        try {
+            const res = await fetch("/")
+            if (!res) return;
+            const data = res.json();
+            if (data.error) {
+                throw new Error(data.error)
+            }
+            console.log(data);
+        } catch (error) {
+            toast.error(error);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    return {getBlogs, loading};
+}
+
+export default useGetBlogs;
